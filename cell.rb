4902673686b9433b664c528599afe415c12c8275
nathancoulter <nathan@coulter.ninja>
2022-11-18 14:50:56 -0500

    wrapped up requirements for 'will be alive'

--- cell.rb
+++ cell.rb
@@ -1,10 +1,15 @@
 class Cell
-    attr_accessor :living_neighbors
-    
+    attr_accessor :living_neighbors, :alive
+
     def alive?
+      alive
     end
 
-    def will_be_alive?
-      living_neighbors == 3  
+    def will_be_alive?  
+      if living_neighbors == 2   
+         alive?
+      else
+        living_neighbors == 3   
+       end
     end
 end
--- cell_test.rb
+++ cell_test.rb
@@ -2,25 +2,39 @@ require_relative "cell"
 require "test/unit"
 
 class CellTest < Test::Unit::TestCase
+  attr_reader :cell
+
+  def setup
+    @cell = Cell.new
+  end
+
   def test_cell_can_be_alive
-    cell = Cell.new
     assert !cell.alive?
   end
 
   def test_will_be_alive_1_neighbor
-    cell = Cell.new
     cell.living_neighbors = 1
     assert !cell.will_be_alive?
   end
 
+  def test_will_be_alive_2_neighbors_currently_alive
+    cell.living_neighbors = 2
+    cell.alive = true
+    assert cell.will_be_alive?
+  end
+
+  def test_will_be_alive_2_neighbors_currently_dead
+    cell.living_neighbors = 2
+    cell.alive = false
+    assert !cell.will_be_alive?
+  end
+
   def test_will_be_alive_3_neighbor
-    cell = Cell.new
     cell.living_neighbors = 3
     assert cell.will_be_alive?
   end
 
   def test_will_be_alive_more_than_3_neighbor
-    cell = Cell.new
     cell.living_neighbors = 4
     assert !cell.will_be_alive?
   end
